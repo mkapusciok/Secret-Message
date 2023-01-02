bf80109063dc9292580d97265379d0c4348739fd
mkapusciok <m.kapusciok@interia.pl>
2023-01-02 12:49:36 +0100

    Check if lenght is smaller than original

--- files/app.js
+++ files/app.js
@@ -1,28 +1,31 @@
 let secretMessage = [
-  "Learning",
-  "is",
-  "not",
-  "about",
-  "what",
-  "you",
-  "get",
-  "easily",
-  "the",
-  "first",
-  "time,",
-  "it",
-  "is",
-  "about",
-  "what",
-  "you",
-  "can",
-  "figure",
-  "out.",
-  "-2015,",
-  "Chris",
-  "Pine,",
-  "Learn",
-  "JavaScript",
-];
-
-secretMessage.pop();
+    "Learning",
+    "is",
+    "not",
+    "about",
+    "what",
+    "you",
+    "get",
+    "easily",
+    "the",
+    "first",
+    "time,",
+    "it",
+    "is",
+    "about",
+    "what",
+    "you",
+    "can",
+    "figure",
+    "out.",
+    "-2015,",
+    "Chris",
+    "Pine,",
+    "Learn",
+    "JavaScript",
+  ];
+  console.log(secretMessage.length);
+  
+  secretMessage.pop();
+  console.log(secretMessage.length);
+  
